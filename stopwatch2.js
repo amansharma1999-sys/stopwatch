a6251a30113c4aad13f4475fdec86cca1194c5c9
amansharma1999-sys <69382601+amansharma1999-sys@users.noreply.github.com>
2022-08-12 08:39:47 +0530

    Add files via upload

---
+++ stopwatch2.js
@@ -0,0 +1,29 @@
+const counter = document.querySelector("h1#counter");
+
+const interval=function(){
+    let val= counter.innerHTML;
+    val++;
+    counter.innerHTML=val;
+}
+
+let intervalId= null;
+
+function start(){
+    if(intervalId) return window.alert("Timer is already running");
+   intervalId= setInterval(interval,1000);
+}
+
+function stop(){
+   if(intervalId) return wimdow.alert("NO timer to stop")
+    clearInterval(intervalId);
+    intervalId=null;
+}
+
+function reset(){
+    if (intervalId){
+        stop();
+    }
+    counter.innerHTML=0;
+
+}
+
